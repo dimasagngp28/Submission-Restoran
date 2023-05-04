13baecaf0a72b7d118654934718e80a08f8a2a74
dimasagngp28 <100175606+dimasagngp28@users.noreply.github.com>
2023-05-04 20:54:45 +0700

    Add files via upload

---
+++ src/scripts/index.js
@@ -0,0 +1,22 @@
+import 'regenerator-runtime'; /* for async await transpile */
+import '../styles/main.css';
+
+console.log('Hello Coders! :)');
+
+const hamburger = document.querySelector('#hamburger');
+const hero = document.querySelector('.hero');
+const main = document.querySelector('main');
+const drawer = document.querySelector('#drawer');
+
+hamburger.addEventListener('click', function (event) {
+  drawer.classList.toggle('open');
+  event.stopPropagation();
+});
+
+hero.addEventListener('click', function () {
+  drawer.classList.remove('open');
+});
+
+main.addEventListener('click', function () {
+  drawer.classList.remove('open');
+});
